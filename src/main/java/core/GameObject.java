055b2e55b80429b2d3ca0b0f398411f612c56fbb
pazik98 <nefedow98@mail.ru>
2023-05-01 18:09:28 +0300

    Изменены классы `Entity` и `GameObject`
    -	Функционал работы с `Component` теперь полностью содержится в классе `Entity`.
    -	Исправлен `.gitignore`.
    -	Реализован метод `getComponents()`.

--- src/main/java/core/GameObject.java
+++ src/main/java/core/GameObject.java
@@ -9,12 +9,10 @@ import java.util.List;
 public abstract class GameObject extends Entity {
 
     private String name;
-    private final List<Component> components;
 
     public GameObject(int id) {
         super(id);
         name = "GameObject " + id;
-        components = new ArrayList<>();
     }
 
     public String getName() {
@@ -25,28 +23,11 @@ public abstract class GameObject extends Entity {
         this.name = name;
     }
 
-    public void addComponent(Component component) {
-        components.add(component);
-    }
-
-    public <T extends Component> T getComponent(Class<T> componentClass) {
-        for (Component component : components) {
-            if (component.getClass().isAssignableFrom(componentClass)) {
-                return componentClass.cast(component);
-            }
-        }
-        return null;
-    }
-
-    public <T extends Component> void removeComponent(Class<T> componentClass) {
-        components.removeIf(component -> component.getClass().isAssignableFrom(componentClass));
-    }
-
     @Override
     public String toString() {
         return "GameObject{" +
                 "name='" + name + '\'' +
-                ", components=" + components +
+                "components=" + getComponents() + '\'' +
                 '}';
     }
 }
--- src/main/java/core/entity/Entity.java
+++ src/main/java/core/entity/Entity.java
@@ -1,14 +1,50 @@
 package core.entity;
 
+import core.component.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
 abstract public class Entity {
 
     private final int id;
+    private final List<Component> components;
 
     public Entity(int id) {
         this.id = id;
+        components = new ArrayList<>();
     }
 
     public int getId() {
         return id;
+    }
+
+    public void addComponent(Component component) {
+        components.add(component);
+    }
+
+    public <T extends Component> T getComponent(Class<T> componentClass) {
+        for (Component component : components) {
+            if (component.getClass().isAssignableFrom(componentClass)) {
+                return componentClass.cast(component);
+            }
+        }
+        return null;
+    }
+
+    public List<Component> getComponents() {
+        return new ArrayList<>(components);
+    }
+
+    public <T extends Component> void removeComponent(Class<T> componentClass) {
+        components.removeIf(component -> component.getClass().isAssignableFrom(componentClass));
+    }
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "id=" + id +
+                ", components=" + components +
+                '}';
     }
 }
